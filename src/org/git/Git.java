c6a65c037469a20565c90a03cdf930b4a8c2e5ff
Manikandaprabu <massmanibrabu@gmail.com>
2021-07-29 16:07:13 +0530

    education added

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -5,9 +5,13 @@ public class Git {
 	private void address() {
 System.out.println("Chennai");
 	}
+	private void education() {
+System.out.println("Green");
+	}
 	public static void main(String[] args) {
 		Git g = new Git();
 		g.address();
+		g.education();
 	}
 
 }
