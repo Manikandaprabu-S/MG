7d7bee03109eff559b1a31a8fcf06f3299261751
Manikandaprabu <massmanibrabu@gmail.com>
2021-07-29 15:56:49 +0530

    Muru

---
+++ src/org/git/Git.java
@@ -0,0 +1,13 @@
+package org.git;
+
+public class Git {
+	
+	private void address() {
+System.out.println("Chennai");
+	}
+	public static void main(String[] args) {
+		Git g = new Git();
+		g.address();
+	}
+
+}
