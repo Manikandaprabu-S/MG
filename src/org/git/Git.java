ec4afb1d97bb954e36bc6c655ae372b16a2f8fe3
Manikandaprabu <massmanibrabu@gmail.com>
2021-07-29 16:19:00 +0530

    ahammed added

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -5,9 +5,13 @@ public class Git {
 	private void address() {
 System.out.println("Chennai");
 	}
+	private void ahammed() {
+System.out.println("ahammed");
+	}
 	public static void main(String[] args) {
 		Git g = new Git();
 		g.address();
+		g.ahammed();
 	}
 
 }
